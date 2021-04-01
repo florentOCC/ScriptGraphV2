07cbe092e2405964949988b0e07d13484d56d356
florentOCC <38077178+florentOCC@users.noreply.github.com>
2021-04-01 11:24:07 +0200

    Update verifVersion.sh

--- verifVersion.sh
+++ verifVersion.sh
@@ -14,12 +14,13 @@ then
 else
 	Version1=$(curl https://raw.githubusercontent.com/florentOCC/ScriptGraphV2/main/Version | cut -d ":" -f 2 )
 	Version2=$(cat ScriptGraphV2-main/Version | cut -d ":" -f 2)
-	if [ Version1 != Version2 ]
+	if [ $Version1 != $Version2 ]
 	then
 		echo "Mise a jour en cours"
 		rm -rf ScriptGraphV2-main
 		rm -rf /usr/share/ombutel/www/graph
 		wget https://github.com/florentOCC/ScriptGraphV2/archive/main.zip
+		sleep 2
 		unzip main.zip
 		rm -rf main.zip
 		cd ScriptGraphV2-main
